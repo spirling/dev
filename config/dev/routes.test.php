b059b07aad004b75096ca36b0bab31acf058556e
asmodeusta <asmodeusta@gmail.com>
2020-03-14 21:56:46 +0200

    Updated: routings

--- config/dev/routes.test.php
+++ config/dev/routes.test.php
@@ -1,13 +1,40 @@
 <?php
 
 return [
-    'site' => [
+    'module' => [
         'path' => '/{module}',
         'requirements' => [
-            'module' => '(site|blog|shop)'
+            'module' => '%activeModules%',
+        ],
+        'defaults' => [
+            'module' => '%defaultModule%',
+        ],
+        'routes' => [
+            'scope' => [
+                'path' => '/{scope}',
+                'requirements' => [
+                    'scope' => '%requiredScopes%',
+                ],
+                'routes' => [
+                    'controller' => [
+
+                    ]
+                ]
+            ],
+            'public' => [
+
+            ],
+        ],
+    ],
+    'two-parts' => [
+        'path' => '/{module}/{action}',
+        'requirements' => [
+            'module' => '(site|blog|shop)',
+            'action' => '(index|edit|view)',
         ],
         'defaults' => [
             'module' => 'site',
+            'action' => 'index',
         ]
     ],
     'default' => [
